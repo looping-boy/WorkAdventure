42bf24723d1fd7825b16419b30c6a400e2c3d0dc
Lotis <75070334+looping-boy@users.noreply.github.com>
2023-01-30 15:36:52 +0100

    Initial commit

---
+++ hello.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="hello" tilewidth="32" tileheight="32" tilecount="850" columns="25">
+ <image source="../WorkAdventure main/workadventure-map-starter-kit/tilesets/WA_Brush2.png" width="800" height="1088"/>
+</tileset>
